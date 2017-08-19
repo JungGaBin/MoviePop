6307715e07bbcdd7db95b8ff3f3223611c691cef
PW486 <ooqwe486@gmail.com>
2017-08-19 21:20:54 +0900

    MoviePop Init

---
+++ movie_serve.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+
+# 모델 불러오기
+sess=tf.Session()
+saver = tf.train.import_meta_graph('movie_model.meta')
+saver.restore(sess,tf.train.latest_checkpoint('./'))
+
+graph = tf.get_default_graph()
+
+X = tf.placeholder(tf.float32, shape=[None, 2])
+keep_prob = tf.placeholder(tf.float32)
+
+W1 = graph.get_tensor_by_name("W1:0")
+B1 = graph.get_tensor_by_name("B1:0")
+L1 = tf.nn.relu(tf.matmul(X, W1) + B1)
+L1 = tf.nn.dropout(L1, keep_prob=keep_prob)
+
+W2 = graph.get_tensor_by_name("W2:0")
+B2 = graph.get_tensor_by_name("B2:0")
+L2 = tf.nn.relu(tf.matmul(L1, W2) + B2)
+L2 = tf.nn.dropout(L2, keep_prob=keep_prob)
+
+W3 = graph.get_tensor_by_name("W3:0")
+B3 = graph.get_tensor_by_name("B3:0")
+L3 = tf.nn.relu(tf.matmul(L2, W3) + B3)
+L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
+
+W4 = graph.get_tensor_by_name("W4:0")
+B4 = graph.get_tensor_by_name("B4:0")
+L4 = tf.nn.relu(tf.matmul(L3, W4) + B4)
+L4 = tf.nn.dropout(L4, keep_prob=keep_prob)
+
+W5 = graph.get_tensor_by_name("W5:0")
+B5 = graph.get_tensor_by_name("B5:0")
+hypothesis = tf.matmul(L4, W5) + B5
+
+print("10.0, 1", sess.run(hypothesis, feed_dict={X: [[10.0, 1]], keep_prob: 1}))
+print("10.0, 0", sess.run(hypothesis, feed_dict={X: [[10.0, 0]], keep_prob: 1}))
+print("5.0, 1", sess.run(hypothesis, feed_dict={X: [[5.0, 1]], keep_prob: 1}))
+print("5.0, 0", sess.run(hypothesis, feed_dict={X: [[5.0, 0]], keep_prob: 1}))
+print("1.0, 1", sess.run(hypothesis, feed_dict={X: [[1.0, 1]], keep_prob: 1}))
+print("1.0, 0", sess.run(hypothesis, feed_dict={X: [[1.0, 0]], keep_prob: 1}))
+
+print("A Taxi Driver - 8.48, 1", sess.run(hypothesis, feed_dict={X: [[8.48, 1]], keep_prob: 1}))
