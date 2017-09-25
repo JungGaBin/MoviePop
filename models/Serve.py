006a036bf049ecbc173e0ec043a34cf455690614
DESKTOP-QV1KKB4\dongpilYu <yoodongphil@naver.com>
2017-09-25 14:04:13 +0900

    Change Serve.py & input_classification

--- models/Serve.py
+++ models/Serve.py
@@ -12,26 +12,24 @@ def SimpleLogistic():
     cls = Classifier(jobject=serialization.read("SimpleLogistic.model"))
     loader = Loader(classname="weka.core.converters.ArffLoader")
     data = loader.load_file("./input_classification.arff")
-    input = np.array([[8148, 42066, 44011723316, 5712639, 1, 1, 7, 30, 1, 8.64, 8.48, 15, 3, 0]])
-    instance = ndarray_to_instances(input, relation="input")
-    print(data)
-    print(instance)
-    instance.class_is_last()
-    for index, inst in enumerate(instance):
-        pred = cls.classify_instance(inst)
-        print("predicted : %d " % inst.get_string_value(inst.class_index),)
+    # 분류이기 때문에 예측 데이터 타입이 nominal이라 numpy로 받을 수 없음.
 
-    return pred;
+    data.class_is_last()
+    for index, inst in enumerate(data):
+        pred = cls.classify_instance(inst)
+        # print("predicted : %d " % inst.get_string_value(inst.class_index),)
+
+    # index가 0부터라 +1
+    return pred + 1;
 
 def SMOreg():
     # load model
     cls = Classifier(jobject=serialization.read("SMOreg.model"))
-    input = np.array([[8148,42066,44011723316,5712639,1,1,7,30,1,8.64,8.48,15,3,0]])
+    input = np.array([[8148,42066,44011723316,5712639,1,1,7,1,8.64,8.48,15,3,0]])
     instance = ndarray_to_instances(input,relation="input")
 
     for index, inst in enumerate(instance):
         pred = cls.classify_instance(inst)
-        print("predicted : %f " %pred)
 
     return pred;
 
@@ -39,12 +37,12 @@ def SMOreg():
 def main():
 
 
-    if False:
+    if True:
         predict = SMOreg()
     else:
         predict = SimpleLogistic()
 
-
+    print(predict)
 
 if __name__ == "__main__":
     try:
